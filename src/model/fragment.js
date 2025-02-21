09959e0481f286ede43514ffff50fee9c8ec9037
Kumudhini Reddicherla <kumudhini.reddicherla@gmail.com>
2025-02-21 17:04:57 -0500

    Added text/markdown

--- src/model/fragment.js
+++ src/model/fragment.js
@@ -103,15 +103,15 @@ class Fragment {
   get formats() {
     const validConversions = {
       'text/plain': ['text/plain'],
-      'text/markdown': ['text/markdown', 'text/html', 'text/plain'],
       'text/html': ['text/html', 'text/plain'],
+      'text/markdown': ['text/markdown', 'text/html', 'text/plain'],
       'text/csv': ['text/csv', 'text/plain', 'application/json'],
       'application/json': ['application/json', 'application/yaml', 'text/plain'],
       'application/yaml': ['application/yaml', 'text/plain'],
       'image/png': ['image/png', 'image/jpeg', 'image/webp', 'image/gif', 'image/avif'],
       'image/jpeg': ['image/png', 'image/jpeg', 'image/webp', 'image/gif', 'image/avif'],
-      'image/webp': ['image/png', 'image/jpeg', 'image/webp', 'image/gif', 'image/avif'],
       'image/avif': ['image/png', 'image/jpeg', 'image/webp', 'image/gif', 'image/avif'],
+      'image/webp': ['image/png', 'image/jpeg', 'image/webp', 'image/gif', 'image/avif'],
       'image/gif': ['image/png', 'image/jpeg', 'image/webp', 'image/gif', 'image/avif'],
     };
     return validConversions[this.mimeType] || false;
